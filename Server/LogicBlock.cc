94fb26b3d4e2934b29eef4c74ba65f2aba10e428
patny205 <patny205@student.liu.se>
2013-11-25 15:49:55 +0100

    LogicBlock should be done.

---
+++ Server/LogicBlock.cc
@@ -0,0 +1,23 @@
+#include "LogicBlock.h"
+
+LogicBlock::LogicBlock(LogicBlock& other) {
+    last_block = other.last_block;
+    next = other.next;
+}
+
+void LogicBlock::set_next(LogicBlock* new_block) {
+    next = new_block;
+}
+
+void LogicBlock::set_last(bool new_value) {
+    last_block = new_value;
+}
+
+LogicBlock* LogicBlock::get_next() {
+    return next;
+}
+
+bool LogicBlock::get_last() const {
+    return last_block;
+}
+
