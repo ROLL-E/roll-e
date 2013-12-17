729dd048e788feeb24070aee480e104161129e92
patny205 <patny205@student.liu.se>
2013-12-17 14:01:15 +0100

    All execpt GUI and network done in Client.

--- Client/main.cc
+++ Client/main.cc
@@ -1,10 +1,42 @@
 #include "clientwindow.h"
 #include <QApplication>
+#include <QList>
+#include <QString>
+#include <QMap>
+#include <iostream>
+#include <QDebug>
+#include "Character.h"
+#include "Item.h"
+#include "Skill.h"
+#include "ClientInventory.h"
 
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   ClientWindow w;
+
+  QMap<QString, qint16> attr_map{{"health", 10}, {"armor", 2}, {"strength", 5}};
+
+  Character* bob(new Character(attr_map, 50));
+
+  bob->set_name("Bob");
+  qDebug() << bob->get_name();
+
+  qDebug() << "bob's health" << bob->get_attribute("health");
+
+  Item* item1(new Item("Hammer of doom"));
+  bob->add_item(item1);
+
+  Item* item2(new Item("nicer hammah"));
+  bob->add_item(item2);
+
+
+  qDebug() << bob->has_item(item1);
+  qDebug() << item1->get_name();
+  qDebug() << item2->get_name();
+
+
+
   w.show();
 
   return a.exec();
--- Client/src/Skill.cc
+++ Client/src/Skill.cc
@@ -1,1 +1,17 @@
+#include "Skill.h"
 
+QMap<QString,qint8> Skill::get_modifiers() const {
+  return modifiers;
+}
+
+QString Skill::get_name() const {
+  return name;
+}
+
+void Skill::set_modifier(const QString& mod_name, qint8 value) {
+  modifiers[mod_name] = value;
+}
+
+void Skill::set_name(const QString& new_name) {
+  name = new_name;
+}
